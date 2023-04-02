070d709229d38f8a835b39f8c646421983aaf673
bfranceschin <bfranceschin@gmail.com>
2023-04-02 08:21:48 -0300

    tailwind config

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active'],
+    },
   },
   plugins: [require("daisyui")],
 };
